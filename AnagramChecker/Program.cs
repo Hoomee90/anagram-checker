d0ce14c5b189e41dac49a5fe9c8bf97035c35e79
Samantha Callie <Hoomee90@gmail.com>
2024-03-21 13:42:18 -0700

    add starting ui logic

--- AnagramChecker/Program.cs
+++ AnagramChecker/Program.cs
@@ -1,10 +1,28 @@
+using System;
+using AnagramChecker.Models;
+
 namespace AnagramChecker
 {
-  class Program
-  {
-    static void Main()
-    {
-      // UI code goes here.
-    }
-  }
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+			Console.WriteLine("Welcome to AnagramChecker");
+			Console.WriteLine("We'll tell you which of a list of words are anagrams of a certain word");
+			Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+			Console.WriteLine("First we need to choose which words we'll be using");
+			MakeAnagram();
+		
+			static void MakeAnagram()
+			{
+				Console.WriteLine("Please enter the word we'll be looking for anagrams of");
+				string anagramWord = Console.ReadLine();
+				Console.WriteLine("Now please enter the list of that word's potential anagrams");
+				Console.WriteLine("Put a space between each word");
+				string anagramList = Console.ReadLine();
+				
+			}
+		}
+	}
 }
