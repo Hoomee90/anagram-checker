ee9113267a2585381c656a2c0d5479bacc184b2d
Samantha Callie <Hoomee90@gmail.com>
2024-03-22 13:42:59 -0700

    add logic for making and editing anagram object

--- AnagramChecker/Program.cs
+++ AnagramChecker/Program.cs
@@ -20,8 +20,33 @@ namespace AnagramChecker
 				string anagramWord = Console.ReadLine();
 				Console.WriteLine("Now please enter the list of that word's potential anagrams");
 				Console.WriteLine("Put a space between each word");
-				string anagramList = Console.ReadLine();
-				
+				string[] anagramList = Console.ReadLine().Split(' ');
+				Anagram anagram = new(anagramWord, anagramList);
+				ConfirmOrEditAnagram(anagram);
+			}
+			
+			static void ConfirmOrEditAnagram(Anagram anagram)
+			{
+				Console.WriteLine("-----------------------------------------");
+				Console.WriteLine("We have collected your input");
+				Console.WriteLine("The chosen word is \"{0}\"", anagram.TestWord);
+				Console.WriteLine("And the words to check are \"{0}\" and \"{1}\"", string.Join(", ", anagram.Guesses, 0, anagram.Guesses.Length - 1), anagram.Guesses[^1]);
+				Console.WriteLine("Is this correct??");
+				Console.WriteLine("'y' to continue, or anything else to reenter your words");
+				string userResponse = Console.ReadLine().ToLower();
+				Console.WriteLine("-----------------------------------------");
+				if (userResponse == "y")
+				{
+					DisplayAnagrams(anagram);
+				} else {
+					Console.WriteLine("Guess those words sucked");
+					MakeAnagram();
+				}
+			}
+			
+			static void DisplayAnagrams(Anagram anagram)
+			{
+
 			}
 		}
 	}
